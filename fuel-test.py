027a98c1fa91cb5ce1a8c65db1610db22f5b9b1e
Jaz White <jasmine.white@vocitec.com>
2019-12-20 15:56:37 -0500

    Adds failing tests for calculating total fuel required for module

--- fuel-test.py
+++ fuel-test.py
@@ -1,7 +1,7 @@
 from ddt import ddt, data, unpack
 import unittest
 
-from fuel import calculate_fuel_of_modules, find_fuel_by_mass
+from fuel import calculate_fuel_for_mass_and_fuel, calculate_fuel_of_modules, find_fuel_by_mass
 
 @ddt
 class TestFeul(unittest.TestCase):
@@ -14,6 +14,13 @@ class TestFeul(unittest.TestCase):
 
         self.assertEqual(fuel, expected_fuel)
 
+    @data([14, 2], [1969, 966], [100756, 50346])
+    @unpack
+    def test_calculate_fuel_for_mass_and_fuel(self, mass, expected_fuel):
+        '''Verify that we calculate the fuel for the fuel'''
+        fuel = calculate_fuel_for_mass_and_fuel(mass)
+
+        self.assertEqual(fuel, expected_fuel)
 
     def test_calculate_fuel_of_modules(self):
         '''Verify that fuel can be calculated for multiple modules'''
--- fuel.py
+++ fuel.py
@@ -5,6 +5,9 @@ def find_fuel_by_mass(mass):
     fuel = round_down(mass / 3) - 2
     return fuel
 
+def calculate_fuel_for_mass_and_fuel(mass):
+    pass
+
 def calculate_fuel_of_modules(modules):
     fuel = 0
     for mass in modules:
