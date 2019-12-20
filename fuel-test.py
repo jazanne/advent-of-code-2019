6addd9ddfc7a3c240db02efd9444dc65240ecc0a
Jaz White <jasmine.white@vocitec.com>
2019-12-20 15:43:45 -0500

    Calculates fuel required for a number of modules

--- fuel-test.py
+++ fuel-test.py
@@ -1,23 +1,23 @@
 from ddt import ddt, data, unpack
 import unittest
 
-from fuel import calculate_fuel_of_modules, find_feul_by_mass
+from fuel import calculate_fuel_of_modules, find_fuel_by_mass
 
 @ddt
 class TestFeul(unittest.TestCase):
 
     @data([12, 2], [14, 2], [1969, 654], [100756, 33583])
     @unpack
-    def test_find_feul_by_mass(self, mass, expected_fuel):
+    def test_find_fuel_by_mass(self, mass, expected_fuel):
         '''Verify that fuel is calculated based on mass'''
-        fuel = find_feul_by_mass(mass)
+        fuel = find_fuel_by_mass(mass)
 
         self.assertEqual(fuel, expected_fuel)
 
 
     def test_calculate_fuel_of_modules(self):
         '''Verify that fuel can be calculated for multiple modules'''
-        modules = [12, 14, 1969, 33583]
+        modules = [12, 14, 1969, 100756]
         expected_fuel = 2 + 2 + 654 + 33583
         fuel = calculate_fuel_of_modules(modules)
 
--- fuel.py
+++ fuel.py
@@ -1,9 +1,12 @@
 from functools import reduce
 from math import floor as round_down
 
-def find_feul_by_mass(mass):
+def find_fuel_by_mass(mass):
     fuel = round_down(mass / 3) - 2
     return fuel
 
 def calculate_fuel_of_modules(modules):
-    pass
+    fuel = 0
+    for mass in modules:
+        fuel += find_fuel_by_mass(mass)
+    return fuel
